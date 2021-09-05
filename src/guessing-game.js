42ba1c1e5be7d47382e7e45700b6b0ec808e3085
Vladislav Paromov <parro@yandex.ru>
2021-09-05 11:03:27 +0300

    feat: guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let mid = Math.floor((this.max + this.min) / 2 + 0.5);
+        return this.mid = mid;
+        
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
